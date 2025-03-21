e6c72ba4c443379f518a87e9ac84d608124d0e16
Matei_Dana <marydanamat@gmail.com>
2025-03-21 23:06:41 +0200

    fix bug

--- data/cart.js
+++ data/cart.js
@@ -16,30 +16,35 @@ function saveToStorage() {
 
 export function addToCart(productId) {
   let matchingItem;
+
   cart.forEach((cartItem) => {
     if (productId === cartItem.productId) {
       matchingItem = cartItem;
-    } 
+    }
   });
+
   if (matchingItem) {
     matchingItem.quantity += 1;
-  }
-  else { 
+  } else {
     cart.push({
       productId: productId,
       quantity: 1
     });
   }
+
   saveToStorage();
 }
 
 export function removeFromCart(productId) {
   const newCart = [];
+
   cart.forEach((cartItem) => {
     if (cartItem.productId !== productId) {
       newCart.push(cartItem);
     }
   });
+
   cart = newCart;
+
   saveToStorage();
 }
