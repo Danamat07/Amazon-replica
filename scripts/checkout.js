122d6b1db5205f22b1c12fb21b0ea68077113995
Matei_Dana <marydanamat@gmail.com>
2025-03-23 21:04:14 +0200

    test some stuff

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,51 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 // import '../data/cart-class.js';
-import {loadProducts} from '../data/products.js';
+import {loadProducts, loadProductsFetch} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
 
 loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+
+/*
+Promise.all([
+  loadproductsFetch(),
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve();
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+
+}).then(() => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
